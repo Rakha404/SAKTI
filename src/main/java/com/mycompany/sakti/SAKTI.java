9b83c0331d269ef045a423dd2e31ece498cb599d
Rakha404 <dawgsyogs@gmail.com>
2026-04-30 08:23:55 +0100

    pertama

---
+++ src/main/java/com/mycompany/sakti/SAKTI.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sakti;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class SAKTI {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
